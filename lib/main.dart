1318fa4cdf32ee2c1d63a31f6b69c92d979f604e
cmstyle <anh1.nguyenquang@toshiba.co.jp>
2023-11-23 15:15:08 +0700

    change title

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Remove Background Image',
       home: const HomeScreen(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
